05ff094a8602f370fdf65559d1082382d624a4e1
Mader Botond <mader.botond@BOLYAI.LOCAL>
2025-02-07 14:51:54 +0100

    ahu

--- datamanger/kitero.js
+++ datamanger/kitero.js
@@ -1,23 +1,12 @@
-const text=document.createElement("input")
-text.setAttribute("type", "text")
-
-const sz=new Number();
-const button=document.createElement("button")
-button.setAttribute("onclick", ()=>{
-    sz=text.innerHTML;
-    TheFunction(sz);
-})
-document.body.appendChild(text)
-document.body.appendChild(button)
-
-
 const obj={};
 obj.calculate=(a, b, cb)=>{
     return cb(a, b);
 }
 console.log(obj.calculate(12, 2, (a, b)=>a/b));
 
-const TheFunction=(szam)=>{
+const TheFunction=()=>{
+    const szam=10;
     const cal=obj.calculate(szam, szam, (a, b)=>a*szam+b);
     console.log(cal);
 }
+TheFunction();
